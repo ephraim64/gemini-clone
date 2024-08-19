047342d77cbba848d9bc5c535d8bf6ec92d045ba
ephraim_tetteh <tettehephraim.64@gmail.com>
2024-08-19 03:50:24 +0000

    Committing to git

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Main from "./components/Main/Main"
+import SideBar from "./components/Sidebar/SideBar"
+
+const App = () => {
+  return (
+    <>
+      <SideBar />
+      <Main />
+    </>
+  )
+}
+
+export default App
